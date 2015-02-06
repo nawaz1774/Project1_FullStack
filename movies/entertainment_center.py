2d3a6ce0e6de29244a69b2fb0bea6410235e91fc
Nawaz Home PC <nawaz1774@gmail.com>
2015-02-06 08:22:42 -0800

    Add rest of the files

---
+++ movies/entertainment_center.py
@@ -0,0 +1,8 @@
+import media
+
+toy_story = media.Movie("Toy Story", "Story of Toys That Come To Life", 
+						"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+						"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print(toy_story.storyline)
+
